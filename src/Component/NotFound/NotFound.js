0bbcd6ac76c47db90263549a3ff97d4439525fac
Khushbu Islam Misbah <khushbuislamoffice@gmail.com>
2021-03-11 17:20:42 +0600

    react-router Assignment-8

---
+++ src/Component/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../NotFound/NotFound.css';
+
+const NotFound = () => {
+    return (
+        <div className = "notFound">
+            <h1>Sorry! Your Page Not Found.</h1>
+            <h3>404 ERROR!!!</h3>
+        </div>
+    );
+};
+
+export default NotFound;
